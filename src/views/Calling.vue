7eef49b09586e583f9c41f2d40e567a1ba68a190
sachinabs <sachinabs.js@gmail.com>
2022-12-15 15:35:28 +0530

    test

--- src/views/Calling.vue
+++ src/views/Calling.vue
@@ -1,29 +1,27 @@
 <template>
-
-    <div class="wrapper animate__animated  animate__slideInUp animate__faster">
-        <div class="continer">
-            <div class="inside-box">
-                <p>-- Connecting --</p>
-            </div>
-
+    <div class="header-unit">
+        <div id="video-container">
+            <video autoplay loop class="fillWidth">
+                <source src="../assets/video.mp4" type='video/webm' />
+            </video>
         </div>
-        <div class="action-buttons">
+    </div>
 
-            <div @click="popTheMessageBox()" class="message-to-hub">
-                <img class="animate__animated animate__rubberBand animate__delay-1s"
-                    src="../assets/icons/mdi_message-group.svg" alt="" srcset="">
-                <span class="rol">Message to Hub</span>
+    <div class="wrap">
+        <div class="video-mini">
+            <video autoplay loop class="mini-video-resource">
+                <source src="../assets/pexels-mikhail-nilov-7682755.mp4" type='video/webm' />
+            </video>
+            <div class="mini-user-name">
+                <span>Rhonda Heath</span>
+                <span><svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path
+                            d="M9.82776 0.721925C9.89347 0.721807 9.95855 0.734641 10.0193 0.759693C10.08 0.784745 10.1352 0.821523 10.1818 0.867925L15.1318 5.81792C15.2255 5.91169 15.2782 6.03884 15.2782 6.17142C15.2782 6.30401 15.2255 6.43116 15.1318 6.52492C14.6518 7.00492 14.0598 7.11292 13.6288 7.11292C13.4518 7.11292 13.2938 7.09492 13.1688 7.07392L10.0348 10.2079C10.1173 10.5403 10.1708 10.8793 10.1948 11.2209C10.2408 11.9229 10.1628 12.9079 9.47476 13.5959C9.381 13.6897 9.25384 13.7423 9.12126 13.7423C8.98868 13.7423 8.86152 13.6897 8.76776 13.5959L5.93876 10.7679L2.75676 13.9499C2.56176 14.1449 1.53776 14.8519 1.34276 14.6569C1.14776 14.4619 1.85476 13.4369 2.04976 13.2429L5.23176 10.0609L2.40376 7.23192C2.31002 7.13816 2.25737 7.01101 2.25737 6.87842C2.25737 6.74584 2.31002 6.61869 2.40376 6.52492C3.09176 5.83692 4.07676 5.75792 4.77876 5.80492C5.12042 5.82886 5.45936 5.8824 5.79176 5.96492L8.92576 2.83192C8.8996 2.67964 8.88622 2.52544 8.88576 2.37092C8.88576 1.94092 8.99376 1.34892 9.47476 0.867925C9.56847 0.774456 9.69541 0.721953 9.82776 0.721925V0.721925ZM9.94976 2.83392V2.83192V2.83392ZM9.94976 2.83192V2.83392C9.97864 2.92189 9.98252 3.01615 9.96098 3.1062C9.93944 3.19625 9.89332 3.27855 9.82776 3.34392L6.29276 6.87792C6.22708 6.94333 6.14451 6.98918 6.05427 7.01037C5.96404 7.03156 5.86969 7.02725 5.78176 6.99792H5.77976L5.76576 6.99392C5.67062 6.96543 5.57457 6.94008 5.47776 6.91792C5.2262 6.85924 4.97041 6.82046 4.71276 6.80192C4.29076 6.77392 3.87676 6.80992 3.53776 6.95192L9.04776 12.4609C9.18876 12.1209 9.22476 11.7079 9.19676 11.2859C9.17128 10.9287 9.1069 10.5752 9.00476 10.2319L9.00076 10.2189V10.2179C8.97123 10.1299 8.96682 10.0353 8.98801 9.94487C9.00921 9.85444 9.05518 9.7717 9.12076 9.70592L12.6568 6.17092C12.7249 6.10242 12.8113 6.0551 12.9057 6.03469C13.0002 6.01428 13.0985 6.02166 13.1888 6.05592L13.2848 6.07792C13.3718 6.09492 13.4928 6.11192 13.6288 6.11192C13.7428 6.11192 13.8588 6.10092 13.9718 6.07192L9.92676 2.02792C9.89776 2.14092 9.88676 2.25792 9.88676 2.37092C9.88725 2.52631 9.90809 2.68096 9.94876 2.83092L9.94976 2.83192Z"
+                            fill="black" />
+                    </svg>
+                </span>
             </div>
-
-            <router-link :to="'/call'">
-                <div class="call-to-hub call-hang ">
-                    <img class="animate__animated animate__rubberBand animate__delay-2s"
-                        src="../assets/icons/Vector (1).svg" alt="" srcset="">
-                    <span class="rol">End the call</span>
-                </div>
-            </router-link>
         </div>
-
     </div>
 
 </template>
@@ -54,28 +52,32 @@ export default {
 }
 </script>
 
-
 <style>
-/* test css */
-
-
-.inside-box {
-    display: flex;
-    background-color: rgba(217, 217, 217, 0.4);
-    border-radius: 21px;
-    width: 1313px;
-    height: 654px;
-    justify-content: center;
-    align-items: center;
-    margin-left: 30px;
-    margin-top: 10px;
+* {
+    margin: 0;
+    padding: 0;
 }
 
-.call-hang {
-    background-color: #D94D46;
+video.fillWidth {
+    position: fixed;
+    top: 50%;
+    left: 50%;
+    min-width: 100%;
+    min-height: 100%;
+    width: auto;
+    height: auto;
+    z-index: -100;
+    -webkit-transform: translateX(-50%) translateY(-50%);
+    transform: translateX(-50%) translateY(-50%);
+    background-size: cover;
+
 }
 
-.call-hang:hover {
-    background-color: #fa3a30;
+.mini-video-resource {
+    width: 220px;
+}
+.video-mini{
+    margin-left: 20px;
+    background-color: aliceblue;
 }
 </style>
